89beeffe9f046dd237ec7ed042aa515f1a38013e
Amina <aminaamangeldi15@mail.ru>
2023-02-12 16:45:01 +0600

    .

--- main.go
+++ main.go
@@ -10,12 +10,12 @@ import (
 func main() {
 	// var ar [] a.Registration
 	d := a.Database{}
-	// r := a.Registration{Name: "Amina", Surname: "Amangeldi", Age: 18, Login: "l", Password: "p"}
+	r := a.Registration{Name: "Amina", Surname: "Amangeldi", Age: 18, Login: "l", Password: "p"}
 	// r1 := assignment1.Registration{Name: "Jin", Password: "Kim", Age: 24, Login: "l1", Password: "p1"}
 	// r.Register(d)
-	// response1 := d.Register(r)
-	// fmt.Println(d)
-	// fmt.Println(response1)
+	response1 := d.Register(r)
+	fmt.Println(response1)
+	fmt.Println(d)
 	// r1.Register(d)
 	au := a.Authorization{Login: "l", Password: "p"}
 	response := au.SignIn(d)
