da4a1d6b5fccda5fe1caeac6a0476a3c88fdbc13
Amina <aminaamangeldi15@mail.ru>
2023-02-20 19:15:09 +0600

    .

--- assignment1/main.go
+++ assignment1/main.go
@@ -10,28 +10,29 @@ import (
 func main() {
 	// var ar [] a.Registration
 	d := a.Database{}
-	// i:= a.Item{"Phone", 150000, 0}
-	
-	// d.AddItem(a.Item{"Phone", 150000, 0})
+	d.AddItem("Phone", 150000)
 	r := a.Registration{Name: "Amina", Surname: "Amangeldi", Age: 18, Login: "l", Password: "p"}
 	// r1 := Registration{Name: "Jin", Password: "Kim", Age: 24, Login: "l1", Password: "p1"}
-	r1 := a.Registration{Name: "Jin", Surname: "Kim", Age: 24, Login: "l1", Password: "p1"}
-	// d.Register(r)
-	// d.Register(r1)
-	r.Register(d)
-	r1.Register(d)
+	r1 := a.Registration{"Jin", "Kim", 24, "l1", "p1"}
+	d.Register(r)
+	d.Register(r1)
 	fmt.Println(d)
-
+	// r1.Register(d)
 	au := a.Authorization{Login: "l", Password: "p"}
-	// response := au.SignIn(d)
-	// fmt.Println(response)
-	au.SignIn(d)
-	// au1 := Authorization{Login: "l1", Password: "p"}
-	// response1 := au1.SignIn(d)
-	// fmt.Println(response1)
-	// d.Register(r)
-	// d.SearchItem("Phone")
-	// Rate(d, au, "Phone", 6)
-	// fmt.Println(d.Items)
+	response := au.SignIn(d)
+	fmt.Println(response)
+	au1 := a.Authorization{Login: "l1", Password: "p1"}
+	response1 := au1.SignIn(d)
+	fmt.Println(response1)
+	// au2 := Authorization{Login: "l1", Password: "e"}
+	// response2 := au2.SignIn(d)
+	// fmt.Println(response2)
+	// // d.Register(r)
+	d.SearchItem("Asus")
+	a.Rate(d, au, "Phone", 6)
+	a.Rate(d, au1, "Phone", 7)
+	fmt.Println(d.Items)
+	// Rate(d, au, "Phone", 10)
 	// d.FilteringItems(70000, 200000, 3, 7)
+	// fmt.Println(d.Items)
 }
--- lec4/main.go
+++ lec4/main.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"fmt"
-	a "github.com/AminaAmangeldi15/Go/lec4/hw"
+	ar "github.com/AminaAmangeldi15/Go/lec4/hw"
 )
 
 func main() {
-	a := a.ArrayList{}
+	a := ar.ArrayList{}
 	a.Add(5)
 	a.Add(6)
 	a.Size()
 	a.Remove(0)
 	a.Size()
 	fmt.Println(a.Get(1))
-	// fmt.Println(a)
-	// v := Vector{}
-	// v.Add(3)
-	// v.Size()
+	fmt.Println(a)
+	v := ar.Vector{}
+	v.Add(3)
+	v.Size()
 }
