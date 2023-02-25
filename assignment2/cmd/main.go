5a5c8f735c050ce0d094e37088415235bc3ba802
Amina <aminaamangeldi15@mail.ru>
2023-02-26 00:20:29 +0600

    .

--- assignment2/cmd/main.go
+++ assignment2/cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	f "github.com/AminaAmangeldi15/Go/assignment2/pkg"
+	m "github.com/AminaAmangeldi15/Go/assignment2/models"
 )
 
 func main(){
@@ -9,17 +10,19 @@ func main(){
 	// if err != nil{
 	// 	panic(err)
 	// }
-	// u := User{"a", "a", "l1", "p1"}
-	// // register(u)
-	// Register(u)
+	// u := m.User{"name", "surname", "lap", "djk"}
+	// f.Register(u)
+	// f.AddItem(m.Item{"Samsung S22", 450000, 0})
+	f.Rate(m.Rating{"Samsung S22", 5})
+	f.SearchItemByName("Samsung S22")
 	// Authorize(Authorization{"ff", "d"})
 	// AddItem(Item{"Iphone 12", 15000, 0})
 	// SearchItemByName("Iphone 12")
 	// Rate(Rating{"Iphone 12", 4})
 	// Rate(Rating{"Iphone 12", 3})
 	// Rate(Rating{"Iphone 12", 2})
-	f.SearchItemByName("Iphone 12")
+	// f.SearchItemByName("Iphone 12")
 	// FilterByPrice(10000, 200000)
-	f.FilterByRating(2, 4)
+	// f.FilterByRating(2, 4)
 
 }
